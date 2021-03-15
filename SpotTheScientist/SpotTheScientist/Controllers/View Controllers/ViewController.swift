f2e7ae495f9c1e8926c0ee50802bb998609f7335
Morley <codymorleyinc@gmail.com>
2021-03-15 13:38:50 -0700

    planes
    set up a blue color plane over recognized images.

--- SpotTheScientist/SpotTheScientist/Controllers/View Controllers/ViewController.swift
+++ SpotTheScientist/SpotTheScientist/Controllers/View Controllers/ViewController.swift
@@ -25,7 +25,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "scientists", bundle: nil) else {
+            fatalError("Couldn't load tracking images.")
+        }
+        
+        configuration.trackingImages = trackingImages
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -37,6 +43,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
     
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
+        
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
+                             height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let planeNode = SCNNode(geometry: plane)
+        
+        let node = SCNNode()
+        node.addChildNode(planeNode)
+        
+        return node
+    }
 }
